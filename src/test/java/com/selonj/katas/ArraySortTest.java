eb059ca7b4d3a37a57e711ed82ef4c6be1c4bb7d
get-started-tdd <32414154@qq.com>
2016-02-19 21:07:15 +0800

    pick the simplest test case for the first test

---
+++ src/test/java/com/selonj/katas/ArraySortTest.java
@@ -0,0 +1,20 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-2-19.
+ */
+public class ArraySortTest {
+    @Test
+    public void anItem() throws Exception {
+        int[] array = new int[]{1};
+
+        ArrayUtil.sort(array);
+
+        assertThat(array,equalTo(new int[]{1}));
+    }
+}
