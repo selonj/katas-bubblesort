1b9f99ca162077c591ab3456dac3f4ef5b19328b
get-started-tdd <32414154@qq.com>
2016-02-22 00:04:35 +0800

    refactoring

--- src/main/java/com/selonj/katas/ArrayUtil.java
+++ src/main/java/com/selonj/katas/ArrayUtil.java
@@ -6,12 +6,15 @@ package com.selonj.katas;
 public class ArrayUtil {
     public static void sort(int[] array) {
         for (int j = 1; j < array.length; j++) {
-            int i = j - 1;
-            if (array[i] > array[j]) {
-                int tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
+            if (array[j - 1] > array[j]) {
+                swap(array, j - 1, j);
             }
         }
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 }
