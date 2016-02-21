1b011ddbb7b4ab7026c28992b83272da809af413
get-started-tdd <32414154@qq.com>
2016-02-22 00:08:50 +0800

    optimize algorithm

--- src/main/java/com/selonj/katas/ArrayUtil.java
+++ src/main/java/com/selonj/katas/ArrayUtil.java
@@ -5,8 +5,8 @@ package com.selonj.katas;
  */
 public class ArrayUtil {
     public static void sort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j < array.length; j++) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 1; j < array.length - i + 1; j++) {
                 if (array[j - 1] > array[j]) {
                     swap(array, j - 1, j);
                 }
