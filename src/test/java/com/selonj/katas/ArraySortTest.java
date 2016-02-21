a5b812abb283bbf50e59efc1744f5ce9565180a7
get-started-tdd <32414154@qq.com>
2016-02-22 00:06:20 +0800

    write the test sort([2,3,1]) ==> [1,2,3]

--- src/test/java/com/selonj/katas/ArraySortTest.java
+++ src/test/java/com/selonj/katas/ArraySortTest.java
@@ -26,6 +26,8 @@ public class ArraySortTest {
         assertSortedArrayEquals(new int[]{1, 3, 2}, new int[]{1, 2, 3});
         assertSortedArrayEquals(new int[]{3, 1, 2}, new int[]{1, 2, 3});
         assertSortedArrayEquals(new int[]{2, 1, 3}, new int[]{1, 2, 3});
+        assertSortedArrayEquals(new int[]{2, 3, 1}, new int[]{1, 2, 3});
+
     }
 
     private void assertSortedArrayEquals(int[] array, int[] expected) {
