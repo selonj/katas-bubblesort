87456993471f06383dc59da0eb546f9ce45355fa
get-started-tdd <32414154@qq.com>
2016-02-21 23:55:42 +0800

    write the first test sort([1]) ==> [1]

---
+++ src/main/java/com/selonj/katas/ArrayUtil.java
@@ -0,0 +1,10 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-2-21.
+ */
+public class ArrayUtil {
+    public static void sort(int[] array) {
+
+    }
+}
---
+++ src/test/java/com/selonj/katas/ArraySortTest.java
@@ -0,0 +1,18 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-2-21.
+ */
+public class ArraySortTest {
+    @Test
+    public void anItem() throws Exception {
+        int []array=new int[]{1};
+        ArrayUtil.sort(array);
+        assertThat(array,equalTo(new int[]{1}));
+    }
+}
